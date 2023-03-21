ca00dcdb86aa2011854bc945f5c5e1013ca6e8ac
AYMZodiac <zodiacvalhalla@gmail.com>
2023-03-21 10:15:29 -0700

    task 3 done

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry number
+ * Discription : 'prints the alphabet in lowercase'
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 97;
+	int i = 65;
+	int k = 36;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	while (i <= 90)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar(k);
+	return (0);
+}
