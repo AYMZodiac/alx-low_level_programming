6f4eb18e9e6a1c11c9fe6839481692fdd39b8e57
AYMZodiac <zodiacvalhalla@gmail.com>
2023-03-22 18:55:28 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry number
+ * Discription : 'prints digit from 0 to 9 with putchar'
+ * Return: 0
+ */
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		putchar(32);
+		putchar(44);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
